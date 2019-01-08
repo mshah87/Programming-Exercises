a1466660ee335ae5e8e07efe83c0f79499460e21
mshah87 <mauliksc87@outlook.com>
2019-01-08 15:35:01 -0500

    organize

---
+++ Data Structures/Princeton Course/algorithms/binarysearch.java
@@ -0,0 +1,32 @@
+public class binarysearch {
+    // logn order of growth
+    public static int binarysearch(int[] n, int key) {
+
+        // both variables are indicies
+        // lo goes right when key is bigger than mid
+        // hi goes left when key is smaller than mids
+        int lo = 0;
+        int hi = n.length - 1;
+
+        while (lo <= hi) {
+
+            // middle index
+            int mid = lo + (hi - lo) / 2;
+
+            if (key < n[mid]) {
+                hi = mid - 1;
+            } else if (key > n[mid]) {
+                lo = mid + 1;
+            } else {
+
+                // if key is not less than or greater than mid, return middle index
+                // which is index of the key
+                return mid;
+            }
+        }
+
+        // key not found
+        return -1;
+
+    }
+}
