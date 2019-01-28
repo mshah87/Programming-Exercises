23b08d09f464b5a88c15842f646cb5f022e0894c
mshah87 <mauliksc87@outlook.com>
2019-01-27 22:51:12 -0500

    use method 2, hashset

--- Interview Prep/Daily Coding Challenge/MicrosoftLongConsecNum.java
+++ Interview Prep/Daily Coding Challenge/MicrosoftLongConsecNum.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /*
 This problem was asked by Microsoft.
 
@@ -8,6 +10,38 @@ For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element seque
 Your algorithm should run in O(n) complexity.
 */
 
-public class MicrosoftLongConsecNum{
+//METHOD 1: sort array then determine the sequence of consecutive numbers
+//Time complexity: O(nlogn, dominated by Arrays.sort()
+//Space: O(1), sorting it in place
+
+//METHOD 2: 
+
+public class MicrosoftLongConsecNum {
+
+    public static int LongConsec(int[] nums){
+
+        //if length 0, then there are no consecutive numbers
+        if(nums.length == 0){
+            return 0;
+        }
+
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        //add all numbers in array to the set
+        for(int i: nums){
+            set.add(i);
+        }
+
+        //variables for left of number and right of number
+        for(int i: nums){
+            int count  = 1;
+            int l = i - 1; 
+            int r = i + 1; 
+
+
+        }
+
     
+        
+    }
 }
