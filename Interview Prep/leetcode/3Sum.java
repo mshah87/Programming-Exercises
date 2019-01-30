9dfd63faa9b0d408d21d8fa01294c928353512a7
mshah87 <mauliksc87@outlook.com>
2019-01-29 21:08:55 -0500

    sort arr

--- Interview Prep/leetcode/3Sum.java
+++ Interview Prep/leetcode/3Sum.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 //#15 - medium
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> list = new Arraylist<List<Integer>>();
 
+
         if(nums == null || nums.length <3){
             return list;
         }
+
+        //sort array
+        Arrays.sort(nums);
+        
+
+
         
 
 
