2d425099a2f8e172c363f137e0577350cd521619
mshah87 <mauliksc87@outlook.com>
2019-02-06 23:10:26 -0500

    use hashmap

--- Interview Prep/Daily Coding Challenge/LyftContiguousElem.java
+++ Interview Prep/Daily Coding Challenge/LyftContiguousElem.java
@@ -1,8 +1,13 @@
-/*abstractThis problem was asked by Lyft.
+import java.util.HashMap;
+
+/*
+
+This problem was asked by Lyft.
 
 Given a list of integers and a number K, return which contiguous elements of the list sum to K.
 
 For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2, 3, 4].
+
 */
 
 //O(n) complexity 
@@ -10,6 +15,13 @@ For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2
 public class LyftContiguousElem{
 
     public static int[] ContiguousElems(int[] nums, int k){
-        return 2;
+
+        int sum = 0;
+        int start = 0;
+        int end = -1;
+
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+
+        return 1;
     }
 }
