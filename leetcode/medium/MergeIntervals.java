6c9d0d2ea9729a4f161a1fc58901b860b07deff8
mshah87 <mauliksc87@outlook.com>
2020-05-07 16:01:24 -0400

    merge intervals

---
+++ leetcode/medium/MergeIntervals.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //Input: [[1,3],[2,6],[8,10],[15,18]]
+        //Output: [[1,6],[8,10],[15,18]]
+        
+        if(intervals.length<=1){
+            return intervals;
+        }
+        
+        Arrays.sort(intervals, (a1, a2) -> Integer.compare(a1[0], a2[0]));
+        
+        List<int[]> result = new ArrayList<>(); //we don't know the size of output array so we use a list
+        int[] currentInterval = intervals[0];
+        result.add(currentInterval);
+        
+        for(int[] interval: intervals){
+            if(currentInterval[1] >= interval[0]){  //if 3>2 in [1,3], [2,6]
+                currentInterval[1] = Math.max(currentInterval[1], interval[1]); //take the max of 3,6 in [1,3], [2,6]
+            } else {
+                currentInterval = interval;
+                result.add(currentInterval);
+            }
+        }
+        return result.toArray(new int[result.size()][]); //return 2d array
+    }
+}
