b0756dbe1ed663cd31fe237e6bbc95ea97eafa77
mshah87 <mauliksc87@outlook.com>
2020-05-09 01:01:00 -0400

    lettercombo

--- leetcode/medium/LetterComboPhoneNum.java
+++ leetcode/medium/LetterComboPhoneNum.java
@@ -1,47 +1,23 @@
 class Solution {
-        public List<String> letterCombinations(String digits) {
-        //BFS METHOD
-         ArrayList<String> result = new ArrayList<String>();
-         if(digits == null || digits.length() == 0){
+    public List<String> letterCombinations(String digits) {
+        //BFS Permutation: O(3^M * 4^M) complexity where M is the number of the digits
+        LinkedList<String> result = new LinkedList<>();
+        if(digits.length() == 0){
              return result;
          }
 
-         HashMap<Character, String> map = new HashMap<Character, String>();   
-         map.put('2', "abc");
-         map.put('3', "def") ;
-         map.put('4', "ghi");
-         map.put('5', "jkl");
-         map.put('6', "mno");
-         map.put('7', "pqrs");
-         map.put('8', "tuv");
-         map.put('9', "wxyz");
-
-         Queue<String> queue = new LinkedList<String>();
-         char c = digits.charAt(0); //get first character of digits (key)
-         String s = map.get(c); //get value of key c
-         
-         for (int i = 0; i < s.length(); i++) {
-              queue.add(s.charAt(i) + ""); //iterate through string and add to queue
-            }
-
-          int size = 0; 
-          for(int i =1; i<digits.length(); i++){
-              c = digits.charAt(i);
-              s = map.get(c);
-              size = queue.size(); //keep track of queue's size
-              for(int j = 0 ; j < size ; j++){
-                  String str = queue.remove(); //removes string from queue
-                  for(int k = 0; k<s.length(); k++){ 
-                      queue.add(str + s.charAt(k)); // concatenates with string of other digit
-                    }
+         String[] values = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; 
+         result.add("");
+        
+        for(int i=0; i<digits.length(); i++){
+            int index = Character.getNumericValue(digits.charAt(i));
+            while(result.peek().length() == i){
+                String perm = result.remove();
+                for(char c: values[index].toCharArray()){
+                    result.add(perm + c);
                 }
             }
-
-            while(!queue.isEmpty()){
-                result.add(queue.remove());
-            }
-
-            return result;
-
         }
+        return result;
     }
+}
