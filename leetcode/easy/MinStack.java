e204f955df8d0aa2d0b65c6397a6e340b0a7b427
mshah87 <mauliksc87@outlook.com>
2020-05-14 21:05:39 -0400

    minstack

---
+++ leetcode/easy/MinStack.java
@@ -0,0 +1,36 @@
+class MinStack {
+    
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    
+    public void push(int x) {
+        if(minStack.isEmpty() || x<=minStack.peek()){
+            minStack.push(x);
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+} 
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
