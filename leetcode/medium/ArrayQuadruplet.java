c06ce04098c0f19ed8f8cd4727f6e89b6086fc77
mshah87 <mauliksc87@outlook.com>
2020-05-22 00:10:45 -0400

    4sum pramp

---
+++ leetcode/medium/ArrayQuadruplet.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+  static int[] findArrayQuadruplet(int[] arr, int s) {
+    // O(n^3) complexity, two pointer approach
+    List<Integer> result = new ArrayList<>();
+
+    Arrays.sort(arr);
+    
+    for(int i = 0; i <arr.length - 3; i++){
+      for(int j = i + 1; j<arr.length - 2 ; j++){
+        int low = j + 1;
+        int high = arr.length - 1;
+        while(low < high) {
+          int sum = arr[i] + arr[j] + arr[low] + arr[high];
+          if(sum == s){
+            result.add(arr[i]);
+            result.add(arr[j]);
+            result.add(arr[low]);
+            result.add(arr[high]);
+            return result.stream().mapToInt(Integer::intValue).toArray();
+          } else if(sum<s){
+            low++;
+          } else {
+            high--;
+          }
+        }
+
+      }
+      
+    }
+    return result.stream().mapToInt(Integer::intValue).toArray();
+  }
+}
