f9216ada8df61e7923c46dc6ddf506e80d6e236f
mshah87 <mauliksc87@outlook.com>
2020-06-04 23:44:05 -0400

    edit class

--- pramp/ShortestWordEditPath.java
+++ pramp/ShortestWordEditPath.java
@@ -12,59 +12,59 @@ class Solution {
 // put : pot
 // big : bit 
 
-static int shortestWordEditPath(String source, String target, String[] words) {
-	// your code goes here
-    HashMap<String, List<String>> map = new HashMap<>();
-    Queue<String> queue = new LinkedList<>();
-    for(String s: words){
-      if(diff(source, s)){
-        if(!map.containsKey(source)){
-          map.put(source, new ArrayList<>());
-        }
-        map.get(source).add(s);
-      }
-    }
-    
-    for(String s: words){
-      for(String s2: words){
-        if(diff(s,s2)){
-          if(!map.containsKey(s)){
-            map.put(s, new ArrayList<>());
+  static int shortestWordEditPath(String source, String target, String[] words) {
+    // your code goes here
+      HashMap<String, List<String>> map = new HashMap<>();
+      Queue<String> queue = new LinkedList<>();
+      for(String s: words){
+        if(diff(source, s)){
+          if(!map.containsKey(source)){
+            map.put(source, new ArrayList<>());
           }
-          map.get(s).add(s2);
+          map.get(source).add(s);
         }
       }
-    }
-    
-    int level = 1; 
-    queue.offer(source);
-    while(!queue.isEmpty()){
-      int size= queue.size();
-      while(size!=0){
-         String word = queue.poll(); 
-         if(word == target){
-          return level;
-         }
-         for(String s: map.get(word)){
-           queue.offer(s);
-         }
-         size--;
+      
+      for(String s: words){
+        for(String s2: words){
+          if(diff(s,s2)){
+            if(!map.containsKey(s)){
+              map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(s2);
+          }
+        }
       }
-      level++;
-    }
-    return -1;
+      
+      int level = 1; 
+      queue.offer(source);
+      while(!queue.isEmpty()){
+        int size= queue.size();
+        while(size!=0){
+          String word = queue.poll(); 
+          if(word == target){
+            return level;
+          }
+          for(String s: map.get(word)){
+            queue.offer(s);
+          }
+          size--;
+        }
+        level++;
+      }
+      return -1;
     }	
-
-    static boolean diff(String a, String b){
-        int count = 0;
-        for(int i = 0; i<a.length(); i++){
-          if(a.charAt(i) != b.charAt(i)){
-            count++;
-          }
-        }
-        if(count == 1){
-          return true;
-        }
-        return false;
+   
+  static boolean diff(String a, String b){
+    int count = 0;
+    for(int i = 0; i<a.length(); i++){
+      if(a.charAt(i) != b.charAt(i)){
+        count++;
+      }
     }
+    if(count == 1){
+      return true;
+    }
+    return false;
+  }
 }
