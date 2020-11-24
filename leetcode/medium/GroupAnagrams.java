e9f2c6882b6aff0f4d93769ab2eb4a3f0dd193f3
mshah87 <mauliksc87@outlook.com>
2020-11-24 14:52:14 -0500

    improve algo

--- leetcode/medium/GroupAnagrams.java
+++ leetcode/medium/GroupAnagrams.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //O(NKlog(K)) complexity, N is # of words and K is length of words
+        //Sorting: O(nmlog(m)) complexity, n is # of words and m is length of words
         if(strs == null || strs.length == 0) return new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
         for(String s: strs){
@@ -11,6 +11,26 @@ class Solution {
                 map.put(key, new ArrayList<>());
             } 
             map.get(key).add(s); // add the unsorted string as value
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //Without sorting: O(m*n) complexity
+        if(strs == null || strs.length == 0) return new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] bucket = new char[26]; //count frequency of s instead of sorting
+            for(char ch: s.toCharArray()){
+                bucket[ch - 'a']++;
+            }
+            String key = new String(bucket); 
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
         }
         return new ArrayList<>(map.values());
     }
