204160e467663b72d4c514fa22e98d8a81aa3dd0
mshah87 <mauliksc87@outlook.com>
2020-11-24 23:43:06 -0500

    new algo for validparen

--- leetcode/easy/ValidParentheses.java
+++ leetcode/easy/ValidParentheses.java
@@ -2,16 +2,18 @@ class Solution { //O(n) complexity, O(n) space
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for(char c: s.toCharArray()){
-            if(c == '(') {
-                stack.push(')');
-            } else if(c == '{') {
-                stack.push('}');
-            } else if(c == '[') {
-                stack.push(']');
-            } else if(stack.isEmpty() || stack.pop() != c){
-                return false;
+            if(c == '(' || c == '{' || c == '[' ){
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+            } else {
+                return false; //if theres only one character
             }
         }
-        return stack.isEmpty(); 
+        return stack.isEmpty(); // in the end stack should be empty
     }
 }
