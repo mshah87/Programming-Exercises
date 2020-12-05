427b4bd1314e2a079ed0d027066d287eb77448d6
mshah87 <mauliksc87@outlook.com>
2020-12-05 16:14:32 -0500

    letter combo

--- leetcode/medium/LetterComboPhoneNum.java
+++ leetcode/medium/LetterComboPhoneNum.java
@@ -1,23 +1,46 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        //BFS Permutation: O(3^M * 4^M) complexity where M is the number of the digits
+        //BFS iterative permutation: O(3^M * 4^M) complexity where M is the number of the digits
+        if(digits.isEmpty() || digits == null) return new LinkedList<>();
         LinkedList<String> result = new LinkedList<>();
-        if(digits.length() == 0){
-             return result;
-         }
-
-         String[] values = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; 
-         result.add("");
         
-        for(int i=0; i<digits.length(); i++){
+        String[] values = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        result.offer("");
+        for(int i = 0; i<digits.length(); i++){
             int index = Character.getNumericValue(digits.charAt(i)); //numeric value of the char
             while(result.peek().length() == i){
-                String perm = result.remove();
+                String perm = result.poll();
                 for(char c: values[index].toCharArray()){
-                    result.add(perm + c);
+                    result.offer(perm + c);
                 }
             }
         }
         return result;
+    }
+}
+
+// Backtracking
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty() || digits == null) return new LinkedList<>();
+        LinkedList<String> result = new LinkedList<>();
+        
+        String[] values = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        backtrack(digits, result, "", values);
+        return result;
+    }
+    
+    public void backtrack(String digits, LinkedList<String> result, String s, String[] values){
+        if(digits.length() == s.length()){
+            result.offer(s);
+            return;
+        }
+        
+        int index = digits.charAt(s.length()) - '0'; // gets numeric value of digit
+        for(char letter: values[index].toCharArray()){
+            backtrack(digits, result, s + Character.toString(letter), values);
+        }
     }
 }
