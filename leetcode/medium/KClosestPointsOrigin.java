538b9f636771ffae9196f8b2fea4efd22b29784b
mshah87 <mauliksc87@outlook.com>
2020-12-08 01:55:43 -0500

    kclosestpointsorigin

---
+++ leetcode/medium/KClosestPointsOrigin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        // O(nlogk) time
+        //euclidean distance
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((p1, p2) -> (p2[0] * p2[0] + p2[1] * p2[1]) - (p1[0] * p1[0] + p1[1] * p1[1]));
+        
+        for(int[] point: points){
+            maxHeap.offer(point);
+            if(maxHeap.size() > K){ // only k elements in maxheap, at the end there will be k minimum elements 
+                maxHeap.poll();
+            }
+        }
+        
+        int[][] result = new int[K][2];
+        while(K-- > 0){
+            result[K] = maxHeap.poll();
+        }
+        return result;
+    }
+}
