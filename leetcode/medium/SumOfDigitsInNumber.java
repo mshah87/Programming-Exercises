48b90cbc8ef61a8aff8077e965132ee38b6950cb
mshah87 <mauliksc87@outlook.com>
2020-12-16 17:05:09 -0500

    more efficient way and comments

--- leetcode/medium/SumOfDigitsInNumber.java
+++ leetcode/medium/SumOfDigitsInNumber.java
@@ -8,9 +8,22 @@ class Solution {
                 n = sum;
                 sum = 0;
             }
-            sum+= n % 10;
-            n = n / 10;
+            sum+= n % 10; // find last digit and add to sum
+            n = n / 10; // remove last digit
         } 
         return n;
+    } 
+}
+
+
+class Solution { 
+    public int digitSum(int n){ // until sum becomes single digit
+        // more efficient O(1) time
+        // 1918 -> 199-> 118 -> 19 -> 10 -> 1
+        if(n == 0) return 0;
+        else if(n % 9 == 0) 
+          return 9;
+        else
+          return n%9;
     } 
 }
