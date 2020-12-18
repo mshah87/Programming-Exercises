61e6ae09852f1819665531f5202c22887c7f0b0a
mshah87 <mauliksc87@outlook.com>
2020-12-17 23:41:36 -0500

    update code

--- leetcode/easy/BestTimeBuySell.java
+++ leetcode/easy/BestTimeBuySell.java
@@ -3,9 +3,10 @@ class Solution {
         if(prices == null || prices.length == 0){
             return 0;
         }
+        
         int max = 0;
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i<prices.length; i++){
+        int min = prices[0];
+        for(int i = 1; i<prices.length; i++){
             min = Math.min(min, prices[i]);
             max = Math.max(max, prices[i] - min);
         }
