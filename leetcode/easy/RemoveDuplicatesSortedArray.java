abfecf317e444d05ec9f5a16370b2b49c04f9dcf
mshah87 <mauliksc87@outlook.com>
2020-12-19 01:46:17 -0500

    small update

--- leetcode/easy/RemoveDuplicatesSortedArray.java
+++ leetcode/easy/RemoveDuplicatesSortedArray.java
@@ -1,18 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         //[1,2,3,3] -> length = 3
-        if(nums.length<2){
-            return nums.length;
-        }
-        int i = 0;
-        int j = 1;
-        while(j<nums.length){
+        if(nums.length == 0) return 0;
+        int i = 0; // slow pointer
+        for(int j = 1; j<nums.length; j++){
             if(nums[i] != nums[j]){
-                i++;
+                i++; // doesn't update current element, always the next
                 nums[i] = nums[j];
             }
-            j++;
         }
-        return i+1;
+        return i + 1; // has all unique elements
     }
 }
