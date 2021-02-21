ad9fde90ea5ad8fc226aaed92f1f9d666a625f87
mshah87 <mauliksc87@outlook.com>
2021-02-21 17:11:59 -0500

    majorityelem

---
+++ leetcode/easy/MajorityElement.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // boyer-moore majority vote algorithm: O(N) time, O(1) space
+        // idea: vote cancels out each occurrence of an element n if we encouneter a diff element, otherwise add on votes
+        // element n will occur till the end if it's the majority element
+        int candidate = nums[0];
+        int count = 1;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if(count == 0){
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return candidate;
+    }
+}
+
+// other methods 
+// 1.) sort and find elem that occurs > n/4 times
+// 2.) hashmap
